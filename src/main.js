820cff384e45af26e8e81d42cf7db6fd60e0eae5
AngusC222 <137439262+AngusC222@users.noreply.github.com>
2023-07-04 14:38:59 +0100

    port removed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express()
-const PORT = 80;    //port 80 maybe in use (try another port)
+const PORT = 80;
 app.use(express.json());
 app.use(
     cors({
